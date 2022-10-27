24a300a73ffe7bf59b6d6a920eeea7e6b0b0fa9c
Dustin Savery <dusave@github.com>
2022-10-27 16:05:35 -0700

    setting up dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { ReactRelayContext } from 'react-relay'
+import { useEnvironment } from '../lib/relay'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const environment = useEnvironment(pageProps.initialRecords)
+
+  return (
+    <ReactRelayContext.Provider value={{ environment }}>
+      <Component {...pageProps} />
+    </ReactRelayContext.Provider>
+  )
 }
